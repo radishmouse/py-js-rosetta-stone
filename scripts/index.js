72947acaf1b88b7d1960ca2979f3290763227789
Chris Aquino <chris.aquino@gmail.com>
2018-10-04 15:58:15 -0400

    does day of week in index.js, moves to madlib.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,46 @@
 
-// Hard-coding instead of prompting
-// because in real life, when you want
-// user input, you'll create an HTML form.
+// Start with a hard-coded value
+// until you know your program works.
+// Then switch to prompting the user.
 
-// Using backticks for hipster strings.
-const name = `chris aquino`;
-const subject = `feline husbandry`;
+let theDay = prompt("Day (0-6)?");
 
+// To convert from a string to a number,
+// use parseInt and provide base 10 as an argument
+theDay = parseInt(theDay);
 
-// Hipster strings let you do linebreaks!
-const finalText = `${name}'s favorite subject in school is ${subject}.
+// const theDay = 6;
+let dayName =`Invalid choice, buddy.`;
 
-How lame.`;
+// You must use parentheses for your condition!!!
+// "Loose" equality is ==
+// "Strict" equality is ===
+// By default, use strict equality, ===
+// "threequals"
+if (theDay === 0) { // And curly braces!
+    // You can leave off the curly braces,
+    // but your "body" can only have one statement.
+    // Any statement after the first will get run
+    // regardless of whether the condition is true or 
+    // not
 
-// Optionally, you can use \n for linebreaks
-// const finalText = `${name}'s favorite subject in school is ${subject}.\n\nHow lame.`;
+    dayName = `Sunday`;
+} else if (theDay === 1) {
+    dayName = `Monday`;
+} else if (theDay === 2) {
+    dayName = `Tuesday`;
+} else if (theDay === 3) {
+    dayName = `Wednesday`;
+} else if (theDay === 4) {
+    dayName = `Thursday`;
+} else if (theDay === 5) {
+    dayName = `Friday`;
+} else if (theDay === 6) {
+    dayName = `Saturday`;
+}
 
-console.log(finalText);
+// The body of an if statement
+// is surrounded by curly braces.
+// And does not require a semi-colon after the curly braces.
+
+console.log(dayName);
---
+++ scripts/madlib.js
@@ -0,0 +1,19 @@
+
+// Hard-coding instead of prompting
+// because in real life, when you want
+// user input, you'll create an HTML form.
+
+// Using backticks for hipster strings.
+const name = `chris aquino`;
+const subject = `feline husbandry`;
+
+
+// Hipster strings let you do linebreaks!
+const finalText = `${name}'s favorite subject in school is ${subject}.
+
+How lame.`;
+
+// Optionally, you can use \n for linebreaks
+// const finalText = `${name}'s favorite subject in school is ${subject}.\n\nHow lame.`;
+
+console.log(finalText);
