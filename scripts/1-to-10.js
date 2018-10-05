b54ce7eb8ccb9409df9463047144179dff4fae19
Chris Aquino <chris.aquino@gmail.com>
2018-10-05 14:55:54 -0400

    moves 1 to 10 to its own file

---
+++ scripts/1-to-10.js
@@ -0,0 +1,5 @@
+
+// Blame Brendan's Boss.
+for (let count = 0; count < 10; count++) {
+    console.log(count);
+}
--- scripts/index.js
+++
@@ -1,5 +0,0 @@
-
-// Blame Brendan's Boss.
-for (let count = 0; count < 10; count++) {
-    console.log(count);
-}
